daee18adb51e79fca89f756885fff7de31a669ca
Maksim1337 <maksimdimitrovmail@gmail.com>
2021-06-15 20:19:25 +0300

    hacky fix clear list key warning

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,10 +1,26 @@
 import Nav from 'react-bootstrap/Nav';
 
 const QuickLinks = ({ links }) => {
-  const navLinks = links.map(link =>
-    <Nav.Item key={link}>
-      <Nav.Link eventKey={link}>{link}</Nav.Link>
-    </Nav.Item>);
+
+  const navLinks = links.map(link => {
+    // hacky fix extract unique key name from an icon
+    const key = () => {
+       if(typeof link === 'string') {
+        return link;
+      } else if(Boolean(link.props.icon.iconName)) {
+        return link.props.icon.iconName;
+      } else {
+        return 'redundantKey';
+      }
+    }
+    
+    return (
+      <Nav.Item key={key()}>
+        <Nav.Link eventKey={key()}>{link}</Nav.Link>
+      </Nav.Item>
+    )
+  })
+
 
   return (
     <Nav>
