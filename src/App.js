14058dabb838332a5becff15399c9bd9bae275c8
Maksim1337 <maksimdimitrovmail@gmail.com>
2021-06-15 20:41:20 +0300

    Dropdown button for the currency

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { facebookIcon, instagramIcon, pinterestIcon, twitterIcon } from './Aweso
 import { TopNavBar } from './NavBar';
 import { QuickLinks } from './Nav';
 
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 function App() {
   const subPageLinks = ['Home', 'Gallery', 'Categories', 'Pages'];
   const socialMediaLinks = [facebookIcon, instagramIcon, pinterestIcon, twitterIcon]
@@ -14,6 +17,12 @@ function App() {
         <QuickLinks links={subPageLinks} />
         <h1 className="brandFont">Shophia</h1>
         <QuickLinks links={socialMediaLinks} />
+        
+        <DropdownButton id="dropdown-basic-button" title="RUPIAH" variant="secondary">
+          <Dropdown.Item href="#/action-1">BGN</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">EUR</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">USD</Dropdown.Item>
+        </DropdownButton>
       </div>
     </>
   );
