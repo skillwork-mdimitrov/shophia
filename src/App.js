02a2aa08fc605516b320e777f14b3b6616715449
Maksim1337 <maksimdimitrovmail@gmail.com>
2021-06-15 14:23:53 +0300

    NavBar extracted in seperate component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import './App.scss';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
+import { NavBar } from './NavBar';
 
 function App() {
-  const phoneNum = "+001 555 801";
-  const phoneIcon = <FontAwesomeIcon icon={faPhoneAlt} />
-
   return (
-    <Navbar bg="light" expand="lg">
-      <div>
-        {phoneIcon}
-        <Navbar.Brand href="#home">Call {phoneNum}</Navbar.Brand>
-      </div>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-        <Navbar.Text>
-          WELCOME MSG!
-        </Navbar.Text>
-        <Nav> 
-          <Nav.Link href="#account">MY ACCOUNT</Nav.Link>
-          <Nav.Link href="#wishlist">MY WISHLIST</Nav.Link>
-          <Nav.Link href="#login">LOGIN</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <NavBar />
   );
 }
 
