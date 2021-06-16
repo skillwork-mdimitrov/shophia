ebbf069b44cbb74260bc91fd280afe3b5c8d8c10
Maksim1337 <maksimdimitrovmail@gmail.com>
2021-06-16 16:48:54 +0300

    Hacky MidSeason sale discount box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { facebookIcon, instagramIcon, pinterestIcon, twitterIcon } from './Aweso
 import Carousel from 'react-bootstrap/Carousel';
 import { CurrencyDropdownBtn } from './DropdownButton';
 import { ReactComponent as BeccaPhotoshoot } from './img/photoShoot-1.svg';
+import { ReactComponent as MidSeasonSale } from './img/mid-season-sale.svg';
 import { TopNavBar } from './NavBar';
 import { QuickLinks } from './Nav';
 
@@ -23,12 +24,21 @@ function App() {
       <Carousel>
         <Carousel.Item>
           <BeccaPhotoshoot alt="First slide" />
+          <Carousel.Caption style={{top: "15%"}}>
+            <MidSeasonSale />
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <BeccaPhotoshoot alt="Second slide" />
+          <Carousel.Caption style={{top: "15%"}}>
+            <MidSeasonSale />
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <BeccaPhotoshoot alt="Third slide" />
+          <Carousel.Caption style={{top: "15%"}}>
+            <MidSeasonSale />
+          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </>
