1d538d75e024f6943d88431f1dc9994f7fa79a8c
Maksim1337 <maksimdimitrovmail@gmail.com>
2021-06-19 15:46:01 +0300

    Featured section WIP + Diamond made more flexible

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './styles/App.scss';
+import Button from 'react-bootstrap/Button';
 import { facebookIcon, instagramIcon, pinterestIcon, twitterIcon } from './AwesomeIcons';
 import { PhotoshootCarousel } from './Carousel';
 import { CurrencyDropdownBtn } from './DropdownButton';
@@ -8,6 +9,8 @@ import { StyledDiamond } from './styles/StyledDiamond';
 import { StyledLinksContainer } from './styles/StyledLinksContainer';
 import { TopNavBar } from './NavBar';
 import { QuickLinks } from './Nav';
+
+import { ReactComponent as BlackBoots } from './img/boots-black.svg';
 
 function App() {
   const subPageLinks = ['Home', 'Gallery', 'Categories', 'Pages'];
@@ -28,15 +31,86 @@ function App() {
 
       <RibbonArea />
 
-      <div className="container-fluid">
+      <div className="container-fluid" style={{ margin: "30px 0" }}>
         <div className="row">
           <hr className="col" />
-          <div className="col">
+          <div className="col d-flex justify-content-around">
+            <StyledDiamond />
+
+            HandPicked
+
             <StyledDiamond />
           </div>
           <hr className="col" />
         </div>
       </div>
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-3">
+            <div className="mb-4" style={{
+              backgroundColor: '#E8E8E8',
+              position: 'relative'
+            }}>
+              <div style={{
+                backgroundColor: 'black',
+                left: '50%',
+                height: '80%',
+                opacity: '0.2',
+                position: 'absolute',
+                top: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: '80%',
+              }}></div>
+              <Button variant="warning" style={{
+                left: '50%',
+                top: '75w%',
+                transform: 'translate(-50%, -50%)',
+              }}>SHOP NOW</Button>{' '}
+              <BlackBoots />
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="mb-4" style={{
+              backgroundColor: '#E8E8E8',
+            }}>
+              <BlackBoots />
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="mb-4" style={{
+              backgroundColor: '#E8E8E8',
+            }}>
+              <BlackBoots />
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-3">
+            <div style={{
+              backgroundColor: '#E8E8E8',
+            }}>
+              <BlackBoots />
+            </div>
+          </div>
+          <div className="col-6">
+            <div style={{
+              backgroundColor: '#E8E8E8',
+            }}>
+              <BlackBoots />
+            </div>
+          </div>
+          <div className="col-3">
+            <div style={{
+              backgroundColor: '#E8E8E8',
+            }}>
+              <BlackBoots />
+            </div>
+          </div>
+        </div>
+      </div>
+
     </>
   );
 }
