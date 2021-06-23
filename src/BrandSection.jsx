7af9523c488b549c35cda0c2304005a1f3612bf1
Maksim1337 <maksimdimitrovmail@gmail.com>
2021-06-23 16:52:05 +0300

    Brand optimized with reusable factory function

--- src/BrandSection.jsx
+++ src/BrandSection.jsx
@@ -5,35 +5,21 @@ import { ReactComponent as LandLockLogo } from './img/brands/landlock-logo.svg';
 import { ReactComponent as LavalierLogo } from './img/brands/lavalier-logo.svg';
 import { ReactComponent as SpectrumLogo } from './img/brands/spectrum-logo.svg';
 
-const classNames = 'col-lg-2 col-md-4 mb-4 col-sm-6 d-flex justify-content-center align-items-center';
+const brandFactory = (logo) =>
+  <div className={"col-lg-2 col-md-4 mb-4 col-sm-6 d-flex justify-content-center align-items-center"}>
+    {logo}
+  </div>
 
 const BrandSection = () =>
   <div className="container-fluid">
     <div className="row">
 
-      <div className={classNames}>
-        <BakeryLogo />
-      </div>
-
-      <div className={classNames}>
-        <LavalierLogo />
-      </div>
-
-      <div className={classNames}>
-        <HarvestLogo />
-      </div>
-
-      <div className={classNames}>
-        <LandLockLogo />
-      </div>
-
-      <div className={classNames}>
-        <SpectrumLogo />
-      </div>
-
-      <div className={classNames}>
-        <HomeEnergyLogo />
-      </div>
+      {brandFactory(<BakeryLogo />)}
+      {brandFactory(<LavalierLogo />)}
+      {brandFactory(<HarvestLogo />)}
+      {brandFactory(<LandLockLogo />)}
+      {brandFactory(<SpectrumLogo />)}
+      {brandFactory(<HomeEnergyLogo />)}
 
     </div>
   </div>
