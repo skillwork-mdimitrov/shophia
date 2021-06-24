c53e5183cfe79d8c6222b38538aa9ee000964749
Maksim1337 <maksimdimitrovmail@gmail.com>
2021-06-24 21:32:50 +0300

    BrandSection extended docs

--- src/components/Brand/BrandSection.jsx
+++ src/components/Brand/BrandSection.jsx
@@ -1,9 +1,9 @@
-import { ReactComponent as BakeryLogo } from '../../img/brands/bakery-logo.svg';
-import { ReactComponent as HarvestLogo } from '../../img/brands/harvest-logo.svg';
-import { ReactComponent as HomeEnergyLogo } from '../../img/brands/home-energy-logo.svg';
-import { ReactComponent as LandLockLogo } from '../../img/brands/landlock-logo.svg';
-import { ReactComponent as LavalierLogo } from '../../img/brands/lavalier-logo.svg';
-import { ReactComponent as SpectrumLogo } from '../../img/brands/spectrum-logo.svg';
+import { ReactComponent as BakeryLogo } from '../../img/Brands/bakery-logo.svg';
+import { ReactComponent as HarvestLogo } from '../../img/Brands/harvest-logo.svg';
+import { ReactComponent as HomeEnergyLogo } from '../../img/Brands/home-energy-logo.svg';
+import { ReactComponent as LandLockLogo } from '../../img/Brands/landlock-logo.svg';
+import { ReactComponent as LavalierLogo } from '../../img/Brands/lavalier-logo.svg';
+import { ReactComponent as SpectrumLogo } from '../../img/Brands/spectrum-logo.svg';
 
 const brandFactory = (logo) =>
   <div className={"col-lg-2 col-md-4 mb-4 col-sm-6 d-flex justify-content-center align-items-center"}>
--- src/components/Brand/Readme.md
+++ src/components/Brand/Readme.md
@@ -1,6 +1,9 @@
-Bootstrap styled responsive component that generates a row of predefined centered brand logos scaling to all device screen sizes.<br>
-Component can be extended to receive logo images externally through props.<br>
-Resize screen to test the responsiveness. 
+A React Bootstrap responsive component that generates a row of centered brand logos scaling to all device screen sizes. 
+
+#### Technical details
+BrandSection component works with <em>.svg</em> images imported as a <i>ReactComponent</i> and passed to the factory function.
+The logo images are spawned in a typical Bootstrap setting (container + row) and are centered within both horizontally and vertically.<br>
+The Bootstrap Grid system is used to control how many will fit the row, based on the device screen size.
 
 ```js
 <BrandSection/>
